1eaa8e474a1ba5036c5acc5cc798be5c0fa1ca8c
allenchau@csu.fullerton.edu <allenchau@csu.fullerton.edu>
2024-11-14 02:22:29 -0800

    queue class (not done)

---
+++ planes_queue.py
@@ -0,0 +1,11 @@
+import queue
+class planes_queue:
+    def __init__(self):
+        pass
+    #Adding our planes to our queue
+    def add_plane(self):
+        pass
+    #Check to see if we have any incoming planes in the queue
+    def check_incoming_planes(self):
+        pass
+        
