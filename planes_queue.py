ba1fa5274d67f0836030c939233c3a3a3e44df99
allenchau@csu.fullerton.edu <allenchau@csu.fullerton.edu>
2024-11-15 16:50:49 -0800

    queue of planes and loop for incoming planes

--- planes_queue.py
+++ planes_queue.py
@@ -1,11 +1,19 @@
 import queue
+import threading
 class planes_queue:
     def __init__(self):
-        pass
+        self.planes = [] #our queue
+
     #Adding our planes to our queue
-    def add_plane(self):
-        pass
+    def add_plane(self, plane_count):
+        for i in range(plane_count):
+            my_current_plane = f"plane{i + 1}"
+            self.planes.append(my_current_plane)
+
     #Check to see if we have any incoming planes in the queue
     def check_incoming_planes(self):
-        pass
-        
+        while self.planes:
+            current_p = self.planes.pop(0)
+            print(f"Our current plane {current_p}")
+            return current_p
+
