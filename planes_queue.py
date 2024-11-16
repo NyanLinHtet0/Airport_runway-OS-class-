28f2fbb83729a5d125c8e2f31533622183dd7085
allenchau@csu.fullerton.edu <allenchau@csu.fullerton.edu>
2024-11-15 19:45:02 -0800

    Updated planes queue

--- planes_queue.py
+++ planes_queue.py
@@ -1,5 +1,6 @@
 import queue
-import threading
+from wind_calculations import Wind
+import random
 class planes_queue:
     def __init__(self):
         self.planes = [] #our queue
@@ -7,13 +8,17 @@ class planes_queue:
     #Adding our planes to our queue
     def add_plane(self, plane_count):
         for i in range(plane_count):
-            my_current_plane = f"plane{i + 1}"
-            self.planes.append(my_current_plane)
+            plane_name = f"plane{i+1}" #labeling each plane created
+            x,y = random.uniform(-5,5), random.uniform(-5,5) #our wind velocity
+            wind = Wind(x,y) #wind object consisting of direction and speed
+            plane_data = {plane_name: wind} #Creating our dictionary
+            self.planes.append(plane_data)
 
     #Check to see if we have any incoming planes in the queue
     def check_incoming_planes(self):
-        while self.planes:
+        if self.planes:
             current_p = self.planes.pop(0)
             print(f"Our current plane {current_p}")
-            return current_p
-
+        else:
+            return None
+        return current_p
